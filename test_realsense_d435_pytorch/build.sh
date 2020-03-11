519b7e47d3ddba7f96edc9c1d0661dc1c6909ba9
Damien Petit <damien.gerard.petit@gmail.com>
2020-03-11 18:50:38 +0900

    Realsense seemse to work

---
+++ test_realsense_d435_pytorch/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#docker build --no-cache -t test_realsense_d435_d .
+docker build -t test_realsense_d435__py_torch_d:dev .
---
+++ test_realsense_d435_pytorch/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xhost +
+
+docker run -it --rm \
+    --gpus all \
+    --privileged \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --network="host"\
+    -e 50000:50003/udp \
+    --name test_realsense_d435__pytorch_d test_realsense_d435__pytorch_d:dev bash
